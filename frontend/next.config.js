fc983ed6bfc0c6ebde514f324125911a347350bc
Sean <seanlengws@gmail.com>
2022-11-04 16:32:19 +0800

    updated next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_TOKEN_SECRET: process.env.NEXT_PUBLIC_TOKEN_SECRET,
+  },
 };
 
 module.exports = nextConfig;
