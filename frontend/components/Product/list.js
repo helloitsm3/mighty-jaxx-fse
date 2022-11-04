d4c5c853528c65cfc7fc8b9d32ec6d715a87eec2
Sean <seanlengws@gmail.com>
2022-11-04 13:27:28 +0800

    added pagination

--- frontend/components/Product/list.js
+++ frontend/components/Product/list.js
@@ -6,11 +6,11 @@ import useLocalStorage from "../../hooks/useLocalStorage";
 import { useApp } from "../../hooks/useApp";
 
 const ProductList = () => {
-  const { appState, setAppState } = useApp();
   const [token, _] = useLocalStorage("user_token");
-  const docs = appState.productlist.docs;
+  const { appState, setAppState } = useApp();
+  const { hasNextPage, hasPrevPage, nextPage, prevPage } = appState.productlist;
 
-  if (docs?.length === 0) {
+  if (appState?.productlist?.docs?.length === 0) {
     return <div>There is currently no product</div>;
   }
 
@@ -28,57 +28,96 @@ const ProductList = () => {
       .catch(() => toast.error("Failed to delete product. Try re-login"));
   };
 
+  const handleNextPage = async () => {
+    const productlist = await axios.get(
+      `http://localhost:3001/product/${nextPage}`
+    );
+
+    setAppState((prev) => ({ ...prev, productlist: productlist.data }));
+  };
+
+  const handlePrevPage = async () => {
+    const productlist = await axios.get(
+      `http://localhost:3001/product/${prevPage}`
+    );
+
+    setAppState((prev) => ({ ...prev, productlist: productlist.data }));
+  };
+
   return (
-    <table className="table-auto w-full">
-      <thead>
-        <tr className="text-left">
-          <th>ID</th>
-          <th>Image</th>
-          <th>SKU</th>
-          <th>Name</th>
-          <th className="text-right">Created at</th>
-          <th className="text-right">Updated at</th>
-          <th className="text-right">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {docs?.map((doc, index) => {
-          const { _id, sku, title, image, createdAt, updatedAt } = doc;
-          return (
-            <tr key={index}>
-              <td>{_id}</td>
-              <td>
-                <img src={image} alt="product image" className="w-32" />
-              </td>
-              <td>{sku}</td>
-              <td>{title}</td>
-              <td className="text-right">{createdAt}</td>
-              <td className="text-right">{updatedAt}</td>
-              <td className="text-right text-white space-x-1">
-                <button
-                  className="bg-blue-500 px-4 py-1 rounded-md"
-                  onClick={() =>
-                    setAppState((prev) => ({
-                      ...prev,
-                      isModalActive: true,
-                      currentEdit: doc,
-                    }))
-                  }
-                >
-                  Edit
-                </button>
-                <button
-                  className="bg-red-500 px-4 py-1 rounded-md"
-                  onClick={() => handleRemove(_id)}
-                >
-                  Remove
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="w-full">
+      <table className="table-auto w-full">
+        <thead>
+          <tr className="text-left">
+            <th>ID</th>
+            <th>Image</th>
+            <th>SKU</th>
+            <th>Name</th>
+            <th className="text-right">Created at</th>
+            <th className="text-right">Updated at</th>
+            <th className="text-right">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appState?.productlist?.docs?.map((doc, index) => {
+            const { _id, sku, title, image, createdAt, updatedAt } = doc;
+
+            return (
+              <tr key={index}>
+                <td>{_id}</td>
+                <td>
+                  <img src={image} alt="product image" className="w-32" />
+                </td>
+                <td>{sku}</td>
+                <td>{title}</td>
+                <td className="text-right">{createdAt}</td>
+                <td className="text-right">{updatedAt}</td>
+                <td className="text-right text-white space-x-1">
+                  <button
+                    className="bg-blue-500 px-4 py-1 rounded-md"
+                    onClick={() =>
+                      setAppState((prev) => ({
+                        ...prev,
+                        isModalActive: true,
+                        currentEdit: doc,
+                      }))
+                    }
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="bg-red-500 px-4 py-1 rounded-md"
+                    onClick={() => handleRemove(_id)}
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <div className="my-10 text-right space-x-5">
+        {hasPrevPage && (
+          <button
+            onClick={handlePrevPage}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          >
+            Prev Page
+          </button>
+        )}
+
+        {hasNextPage && (
+          <button
+            onClick={handleNextPage}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          >
+            Next Page
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
 
